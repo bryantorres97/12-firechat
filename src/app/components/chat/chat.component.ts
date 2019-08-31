cd66d4ccbe96355e6243fec26a65df0b51d15f6a
Bryan Torres <bryantorresckp@gmail.com>
2019-08-31 16:57:41 -0500

    Enviando mensajes  firebase

--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ChatService } from '../../providers/chat.service';
+import { Mensaje } from '../../interfaces/mensaje.interface';
+
 
 @Component({
   selector: 'app-chat',
@@ -6,12 +9,22 @@ import { Component } from '@angular/core';
   styles: []
 })
 export class ChatComponent {
-
+  chats: Mensaje[] = [];
   mensaje = '';
-  constructor() { }
+  constructor(public chatService: ChatService) {
+    this.chatService.cargarMensajes().subscribe();
+    // chats = this.chattS
+  }
 
   enviarMensaje() {
     console.log(this.mensaje);
+    if (this.mensaje.trim().length === 0) {
+      return;
+    }
+
+    this.chatService.agregarMensaje( this.mensaje )
+    .then(() => this.mensaje = '')
+    .catch((error) => console.error('No se pudo enviar el mensaje', error));
   }
 
 }
--- src/app/providers/chat.service.ts
+++ src/app/providers/chat.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { Mensaje } from '../interfaces/mensaje.interface';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ChatService {
 
-  constructor() { }
+  public chats: Mensaje[] = [];
+
+  private itemsCollection: AngularFirestoreCollection<Mensaje>;
+
+  constructor(private afs: AngularFirestore) { }
+
+  cargarMensajes() {
+    this.itemsCollection = this.afs.collection<Mensaje>('chats');
+    return this.itemsCollection.valueChanges().pipe(map((mensajes: Mensaje[]) => {
+      console.log(mensajes);
+      this.chats = mensajes;
+    }));
+  }
+
+  agregarMensaje(texto: string) {
+    const mensaje: Mensaje = {
+      nombre: 'Demo',
+      mensaje: texto,
+      fecha: new Date().getTime()
+      // TODO generar el UID
+    };
+    return this.itemsCollection.add(mensaje);
+  }
 }
